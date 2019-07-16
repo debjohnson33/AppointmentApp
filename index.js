3f08892f4b7936c91e0ac960c018e06f136045a2
debjohnson33 <debjohnson_11@yahoo.com>
2019-07-16 15:06:14 -0500

    Edits link in info/adds infoWindow load from file

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ var BrowserWindow = electron.BrowserWindow;
 var app = electron.app;
 
 app.on('ready', function() {
-    var appWindow;
+    var appWindow, infoWindow;
     appWindow = new BrowserWindow();
     appWindow.loadURL('http://raybo.org');
+    infoWindow = new BrowserWindow({
+        width: 400,
+        height: 300
+    });
+    infoWindow.loadURL('file://' + __dirname + '/info.html');
 });
