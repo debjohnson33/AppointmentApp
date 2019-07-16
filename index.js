9e1a5c29d6db12708bda625856f012783c7d3485
debjohnson33 <debjohnson_11@yahoo.com>
2019-07-16 15:11:46 -0500

    Sets ready & timeouts for windows

--- index.js
+++ index.js
@@ -4,11 +4,23 @@ var app = electron.app;
 
 app.on('ready', function() {
     var appWindow, infoWindow;
-    appWindow = new BrowserWindow();
+    appWindow = new BrowserWindow({
+        show: false
+    });
     appWindow.loadURL('http://raybo.org');
     infoWindow = new BrowserWindow({
         width: 400,
-        height: 300
+        height: 300,
+        show: false
     });
     infoWindow.loadURL('file://' + __dirname + '/info.html');
+
+    appWindow.once('ready-to-show', function () {
+        appWindow.show();
+        setTimeout(function() {
+            infoWindow.show();
+            setTimeout(function() {infoWindow.hide();}, 3000);
+        }, 1000)
+        
+    });
 });
