072407159e27c02d6b6bde7c0ddf3bad39ab401d
debjohnson33 <debjohnson_11@yahoo.com>
2019-07-16 21:38:06 -0500

    Edits to fix bug

--- app/main.js
+++ app/main.js
@@ -4,24 +4,29 @@ var app = electron.app;
 var ipc = electron.ipcMain;
 
 app.on('ready', function() {
-    var appWindow, infoWindow;
-    appWindow = new BrowserWindow({
-        show: false
-    });
-    appWindow.loadURL('file://' + __dirname + '/index.html');
-    infoWindow = new BrowserWindow({
-        width: 400,
-        height: 300,
-        show: false
-    });
-    infoWindow.loadURL('file://' + __dirname + '/info.html');
+  var appWindow, infoWindow;
+  appWindow = new BrowserWindow({
+    show: false
+  }); //appWindow
 
-    appWindow.once('ready-to-show', function () {
-        appWindow.show();
-    });
+  appWindow.loadURL('file://' + __dirname + '/index.html');
 
-    ipc.on('closeInfoWindow', function(event) {
-        event.returnValue = '';
-        infoWindow.hide();
-    });
-});
+  infoWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    transparent: true,
+    show: false,
+    frame: false
+  }); //infoWindow
+
+  infoWindow.loadURL('file://' + __dirname + '/info.html');
+
+  appWindow.once('ready-to-show', function() {
+    appWindow.show();
+  }); //ready-to-show
+
+  ipc.on('closeInfoWindow', function(event, arg){
+    event.returnValue='';
+    infoWindow.hide();
+  }); //closeInfoWindow
+}); //app is ready
