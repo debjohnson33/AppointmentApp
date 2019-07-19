39f10dc95decd5ea0a00ea16f6ae98ece1511617
debjohnson33 <debjohnson_11@yahoo.com>
2019-07-18 19:51:14 -0500

    Adds HeaderNav/search functionality

--- process/js/render.js
+++ process/js/render.js
@@ -12,11 +12,13 @@ var ReactDOM = require('react-dom');
 var AptList = require('./AptList');
 var Toolbar = require('./Toolbar');
 var AddAppointment = require('./AddAppointment');
+var HeaderNav = require('./HeaderNav')
 
 var MainInterface = React.createClass({
   getInitialState: function() {
     return {
       aptBodyVisible: false,
+      queryText: '',
       myAppointments: loadApts
     }//return
   }, //getInitialState
@@ -54,15 +56,33 @@ var MainInterface = React.createClass({
         myAppointments: newApts
     });
   },
+  searchApts: function(query) {
+    this.setState({
+        queryText: query
+    })
+  },
   render: function() {
+    var filteredApts = [];
+    var queryText = this.state.queryText;
     var myAppointments = this.state.myAppointments;
+
+    for (var i = 0; i < myAppointments.length; i++) {
+        if ( 
+            (myAppointments[i].petName.toLowerCase().indexOf(queryText)!=-1) ||
+            (myAppointments[i].ownerName.toLowerCase().indexOf(queryText)!=-1) ||
+            (myAppointments[i].aptDate.toLowerCase().indexOf(queryText)!=-1) ||
+            (myAppointments[i].aptNotes.toLowerCase().indexOf(queryText)!=-1)
+          ) {
+            filteredApts.push(myAppointments[i]);
+          }
+    }
 
     if(this.state.aptBodyVisible === true) {
         $('#addAppointment').modal('show');
     } else {
         $('#addAppointment').modal('hide');
     }
-    myAppointments = myAppointments.map(function(item, index) {
+    filteredApts = filteredApts.map(function(item, index) {
         return(
             <AptList 
                 key={index} 
@@ -76,6 +96,9 @@ var MainInterface = React.createClass({
     return(
 
       <div className="application">
+          <HeaderNav
+            onSearch={this.searchApts}
+          />
         <div className="interface">
             <Toolbar 
                 handleToggle={this.toggleAptDisplay}
@@ -89,7 +112,7 @@ var MainInterface = React.createClass({
             <div className="row">
             <div className="appointments col-sm-12">
                 <h2 className="appointments-headline">Current Appointments</h2>
-                <ul className="item-list media-list">{myAppointments}</ul>
+                <ul className="item-list media-list">{filteredApts}</ul>
             </div>{/* col-sm-12 */}
             </div>{/* row */}
             </div>{/* container */}
